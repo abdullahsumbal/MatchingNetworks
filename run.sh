04b8f08bd706783216d18484fa33d0c706aca63d
abdullahsumbal <abdullahsumbal@gmail.com>
2019-04-07 06:40:43 +0000

    added script for yolofying

--- run.sh
+++ run.sh
@@ -8,10 +8,13 @@ for n_way in "${n_way_list[@]}" #iter over n_ways_list
 do
   for n_shot in "${n_shot_list[@]}" # iter over n_shot_list
   do
-    echo $n_way $n_shot
+    echo "====================================================================================="
+    echo "                       Script for $n_way n_way and $n_shot n_shot                            "
+    echo "-------------------------------------------------------------------------------------"
+
     command="python3 train_one_shot_learning_matching_network.py --batch_size 32 --experiment_title omniglot_"$n_way"_"$n_shot"_matching_network --total_epochs $epochs --full_context_unroll_k 5 --classes_per_set $n_way --samples_per_class $n_shot --use_full_context_embeddings False --use_mean_per_class_embeddings False --dropout_rate_value 0.0"
     echo $command
+    $command
   done
 
 done
-# python3 train_one_shot_learning_matching_network.py --batch_size 32 --experiment_title omniglot_20_1_matching_network --total_epochs 10 --full_context_unroll_k 5 --classes_per_set 20 --samples_per_class 1 --use_full_context_embeddings False --use_mean_per_class_embeddings False --dropout_rate_value 0.0
